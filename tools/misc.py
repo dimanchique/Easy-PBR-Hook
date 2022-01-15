43a75961a0fb6b44819d79ae0796356d486e5cd8
unknown <ghettorunner@yandex.ru>
2022-01-15 20:41:59 +0300

    fix path

--- tools/misc.py
+++ tools/misc.py
@@ -5,7 +5,7 @@ __all__ = ['TEXTURES_MASK', 'TEXTURES']
 
 def read_texture_masks():
     textures = {}
-    with open(bpy.utils.user_resource('SCRIPTS', "addons") + '\\Easy-PBR-Hook\\tools\\mask.dat', 'r') as file:
+    with open(bpy.utils.user_resource('SCRIPTS', "addons") + '\\Easy-PBR-Hook-multifile_beta\\tools\\mask.dat', 'r') as file:
         masks = file.readlines()
     for line in masks:
         key = line.split(':')[0]
