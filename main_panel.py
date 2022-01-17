e475d974b6ddabb47e1d0f86e634c2f9027be6c3
dimanchique <ghettorunner@yandex.ru>
2022-01-17 17:56:45 +0300

    unnecessary functions removed; logic fix

--- main_panel.py
+++ main_panel.py
@@ -13,60 +13,36 @@ class PBRPanel(bpy.types.Panel):
 
     @classmethod
     def poll(cls, context):
-        if not context.active_object:
+        if not context.selected_objects:
             return False
-        elif context.active_object.active_material is None:
+        elif not context.active_object.active_material:
             return False
-        else:
-            if 'CURRENT' not in Material.MATERIALS:
-                Material(context.selected_objects[0].active_material.name)
-            return True
+        return True
 
     def draw(self, context):
         layout = self.layout
-        if PBRPanel.is_bad_state(layout):
-            return
         material_prop = context.active_object.active_material.props
-        Material.MATERIALS['CURRENT'].current_path = material_prop.conf_path
-        Material.MATERIALS['CURRENT'].current_pattern = material_prop.texture_pattern
-        PBRPanel.update_material()
+        PBRPanel.update_material(context)
         row = layout.row()
-        row.prop(material_prop, "conf_path")
+        row.prop(material_prop, "textures_path")
         row = layout.row()
-        row.prop(material_prop, "texture_pattern")
+        row.prop(material_prop, "textures_pattern")
         row = layout.row()
         row.prop(material_prop, "UseMaterialNameAsKeyword")
         row = layout.row()
         row.operator(DBUpdateMenu.bl_idname, text='Update textures masks')
-        PBRPanel.assign_textures(layout)
+        if context.active_object.active_material.props.textures_path != "":
+            PBRPanel.assign_textures(layout)
 
-    @staticmethod
-    def update_material():
-        material_name = bpy.context.selected_objects[0].active_material.name
-        if 'CURRENT' not in Material.MATERIALS:
-            Material(material_name)
-        elif Material.MATERIALS['CURRENT'].name != material_name:
-            if material_name not in Material.MATERIALS:
-                Material(material_name)
-            else:
-                Material.MATERIALS['CURRENT'] = Material.MATERIALS[material_name]
+    @classmethod
+    def update_material(cls, context):
+        material = context.active_object.active_material
+        Material.check_material(material.name)
+        Material.MATERIALS['CURRENT'].current_path = material.props.textures_path
+        Material.MATERIALS['CURRENT'].current_pattern = material.props.textures_pattern
 
-    @staticmethod
-    def is_bad_state(layout):
-        if not bpy.context.selected_objects:
-            row = layout.row()
-            row.label(text="No active object")
-            return True
-        if bpy.context.selected_objects[0].active_material is None:
-            row = layout.row()
-            row.label(text="No active material")
-            if 'CURRENT' in Material.MATERIALS:
-                Material.MATERIALS['CURRENT'].finished = False
-            return True
-        return False
-
-    @staticmethod
-    def assign_textures(layout):
+    @classmethod
+    def assign_textures(cls, layout):
         row = layout.row()
         text = "Reassign textures" if Material.MATERIALS['CURRENT'].finished else "Assign textures"
         row.operator(GetTextureOperator.bl_idname, text=text)
