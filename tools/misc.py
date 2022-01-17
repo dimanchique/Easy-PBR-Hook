e5792706669bfd5fe2e062671a4469cbc8bbecf7
dimanchique <ghettorunner@yandex.ru>
2022-01-17 17:55:00 +0300

    added coloring dict; added warning strings

--- tools/misc.py
+++ tools/misc.py
@@ -1,6 +1,6 @@
 import os
 
-__all__ = ['TEXTURES_MASK', 'TEXTURES']
+__all__ = ['TEXTURES_MASK', 'TEXTURES', 'TEXTURES_COLORS', 'UV_MAP_WARNING_MESSAGE', 'TEXTURE_GETTER_WARNING_MESSAGE']
 
 
 def read_texture_masks():
@@ -15,5 +15,23 @@ def read_texture_masks():
 
 TEXTURES_MASK = read_texture_masks()
 
-
 TEXTURES = list(TEXTURES_MASK.keys())
+
+UV_MAP_WARNING_MESSAGE = 'UV Map not found. Please, fix this problem to use Texture coordinates section!'
+TEXTURE_GETTER_WARNING_MESSAGE = 'No textures found! Check path and keyword'
+
+TEXTURES_COLORS = {'Albedo': 'sRGB',
+                   'Metal Smoothness': 'Non-Color',
+                   'Metal': 'Non-Color',
+                   'Roughness': 'Non-Color',
+                   'ORM': 'Non-Color',
+                   'Color Mask': 'Non-Color',
+                   'Normal Map': 'Non-Color',
+                   'Emission': 'sRGB',
+                   'Specular': 'sRGB',
+                   'Occlusion': 'sRGB',
+                   'Displacement': 'Non-Color',
+                   'Opacity': 'Non-Color',
+                   'Detail Map': 'Non-Color',
+                   'Detail Mask': 'Non-Color'
+                   }
