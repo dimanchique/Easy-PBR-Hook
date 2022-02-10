2471d070bed16720b18253f3af5f13cb7fdfc81b
dimanchique <ghettorunner@yandex.ru>
2022-02-10 14:17:33 +0300

    fixed little issues with import/export operators

--- tools/global_tools.py
+++ tools/global_tools.py
@@ -7,61 +7,70 @@ __all__ = ['Tools']
 
 
 class Tools(bpy.types.PropertyGroup):
-    ###################################################################################################################
-    # TEXTURE MASKS
-    ###################################################################################################################
-    TEXTURES_MASK = {}
-    TEXTURES = []
-    TEXTURES_COLORS = {}
+
+    TEXTURES_KEYWORDS_DICT = {}
+    TEXTURE_TYPES = []
+    TEXTURES_COLORING = {}
     PROP_TO_TEXTURE = {}
+    MESSAGES = {}
+
+    @staticmethod
+    def validate_path(filename, path):
+        if path:
+            path = os.path.join(path, filename)
+        else:
+            path = os.path.join(os.path.dirname(os.path.realpath(__file__)), filename) + '.json'
+        return path
+
+    @classmethod
+    def read_json_data(cls, filename, path=None):
+        path = cls.validate_path(filename, path)
+        with open(path, 'r') as file:
+            return json.load(file)
+
+    @classmethod
+    def write_json_data(cls, data, filename, path=None):
+        path = cls.validate_path(filename, path)
+        with open(path, 'w') as file:
+            json.dump(data, file)
+
+    @classmethod
+    def write_textures(cls, path=None):
+        cls.write_json_data(cls.TEXTURES_KEYWORDS_DICT, 'texture_mask', path)
+        cls.read_textures(path)
+        cls.update_panel_masks()
+
+    @classmethod
+    def read_textures(cls, path=None):
+        cls.TEXTURES_KEYWORDS_DICT = cls.read_json_data('texture_mask', path)
+        cls.update_panel_masks()
 
     @classmethod
     def update_files(cls):
         cls.read_textures()
-        with open(os.path.dirname(os.path.realpath(__file__)) + '\\texture_colors.json', 'r') as file:
-            cls.TEXTURES_COLORS = json.load(file)
+        cls.TEXTURE_TYPES = list(cls.TEXTURES_KEYWORDS_DICT.keys())
+        cls.TEXTURES_COLORING = cls.read_json_data('texture_colors')
+        cls.PROP_TO_TEXTURE = cls.read_json_data('prop_to_texture_match')
+        cls.MESSAGES = cls.read_json_data('user_messages')
 
-        with open(os.path.dirname(os.path.realpath(__file__)) + '\\prop_to_texture_match.json', 'r') as file:
-            cls.PROP_TO_TEXTURE = json.load(file)
-
-    @staticmethod
-    def update_panel_masks():
+    @classmethod
+    def update_panel_masks(cls):
         if hasattr(bpy.context, 'scene') and hasattr(bpy.context.scene, 'db_strings'):
-            bpy.context.scene.db_strings.albedo = ', '.join(Tools.TEXTURES_MASK['Albedo'])
-            bpy.context.scene.db_strings.met_sm = ', '.join(Tools.TEXTURES_MASK['Metal Smoothness'])
-            bpy.context.scene.db_strings.metal = ', '.join(Tools.TEXTURES_MASK['Metal'])
-            bpy.context.scene.db_strings.rough = ', '.join(Tools.TEXTURES_MASK['Roughness'])
-            bpy.context.scene.db_strings.orm = ', '.join(Tools.TEXTURES_MASK['ORM'])
-            bpy.context.scene.db_strings.color_mask = ', '.join(Tools.TEXTURES_MASK['Color Mask'])
-            bpy.context.scene.db_strings.normal_map = ', '.join(Tools.TEXTURES_MASK['Normal Map'])
-            bpy.context.scene.db_strings.emission = ', '.join(Tools.TEXTURES_MASK['Emission'])
-            bpy.context.scene.db_strings.specular = ', '.join(Tools.TEXTURES_MASK['Specular'])
-            bpy.context.scene.db_strings.occlusion = ', '.join(Tools.TEXTURES_MASK['Occlusion'])
-            bpy.context.scene.db_strings.displacement = ', '.join(Tools.TEXTURES_MASK['Displacement'])
-            bpy.context.scene.db_strings.opacity = ', '.join(Tools.TEXTURES_MASK['Opacity'])
-            bpy.context.scene.db_strings.detail_map = ', '.join(Tools.TEXTURES_MASK['Detail Map'])
-            bpy.context.scene.db_strings.detail_mask = ', '.join(Tools.TEXTURES_MASK['Detail Mask'])
+            bpy.context.scene.db_strings.albedo = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Albedo'])
+            bpy.context.scene.db_strings.met_sm = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Metal Smoothness'])
+            bpy.context.scene.db_strings.metal = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Metal'])
+            bpy.context.scene.db_strings.rough = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Roughness'])
+            bpy.context.scene.db_strings.orm = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['ORM'])
+            bpy.context.scene.db_strings.color_mask = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Color Mask'])
+            bpy.context.scene.db_strings.normal_map = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Normal Map'])
+            bpy.context.scene.db_strings.emission = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Emission'])
+            bpy.context.scene.db_strings.specular = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Specular'])
+            bpy.context.scene.db_strings.occlusion = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Occlusion'])
+            bpy.context.scene.db_strings.displacement = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Displacement'])
+            bpy.context.scene.db_strings.opacity = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Opacity'])
+            bpy.context.scene.db_strings.detail_map = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Detail Map'])
+            bpy.context.scene.db_strings.detail_mask = ', '.join(Tools.TEXTURES_KEYWORDS_DICT['Detail Mask'])
 
-    @staticmethod
-    def write_textures(path=None):
-        if not path:
-            path = os.path.dirname(os.path.realpath(__file__)) + '\\texture_mask.json'
-        with open(path, 'w') as file:
-            json.dump(Tools.TEXTURES_MASK, file)
-
-    @staticmethod
-    def read_textures(path=None):
-        if not path:
-            path = os.path.dirname(os.path.realpath(__file__)) + '\\texture_mask.json'
-        with open(path, 'r') as file:
-            Tools.TEXTURES_MASK = json.load(file)
-            Tools.TEXTURES = list(Tools.TEXTURES_MASK.keys())
-        Tools.write_textures()
-        Tools.update_panel_masks()
-
-    ###################################################################################################################
-    # DB UPDATING
-    ###################################################################################################################
     @staticmethod
     def local_update():
         data = dict(bpy.context.scene.db_strings.items())
@@ -69,49 +78,32 @@ class Tools(bpy.types.PropertyGroup):
             if item in Tools.PROP_TO_TEXTURE:
                 line = list(map(str.strip, data[item].lower().split(',')))
                 line = [i for i in line if i != '']
-                Tools.TEXTURES_MASK[Tools.PROP_TO_TEXTURE[item]] = list(set(line))
+                Tools.TEXTURES_KEYWORDS_DICT[Tools.PROP_TO_TEXTURE[item]] = list(set(line))
         Tools.update_panel_masks()
 
     @staticmethod
     def global_update():
         Tools.local_update()
         Tools.write_textures()
-    ###################################################################################################################
-    # MESSAGES
-    ###################################################################################################################
-    UV_MAP_WARNING_MESSAGE = 'UV Map not found. Please, fix this problem to use Texture coordinates section!'
-    TEXTURE_GETTER_WARNING_MESSAGE = 'No textures found! Check path and keyword'
-    UPDATE_DATABASE = {'Local': "Database was locally updated!",
-                       'Global': "Database was globally updated!"}
-    IMAGE_UPDATE = 'Images were updated!'
-    DETAIL_MASK_PLACED = 'Detail Mask node was created'
-    DETAIL_MASK_REMOVED = 'Detail Mask node was removed'
-    SUCCESS_LOADING = 'Textures loaded successfully!'
-    EXPORT_FINISHED = 'Masks exported successfully!'
-    IMPORT_FINISHED = 'Masks imported successfully!'
-    IMPORT_ERROR = 'Unsupported file!'
 
     @classmethod
     def register(cls):
         cls.update_files()
+        cls.write_json_data(cls.TEXTURES_KEYWORDS_DICT, 'default_texture_mask')
 
 
 class DataExporter(bpy.types.Operator, ExportHelper):
     bl_idname = "pbr.export_data"
     bl_label = "Export User Masks"
 
-    # ExportHelper mixin class uses this
     filename_ext = ".json"
-
-    filter_glob = bpy.props.StringProperty(
-            default="*.json",
-            options={'HIDDEN'},
-            maxlen=255,  # Max internal buffer length, longer would be clamped.
-            )
+    filter_glob = bpy.props.StringProperty(default="*.json", options={'HIDDEN'}, maxlen=255)
 
     def execute(self, context):
-        Tools.write_textures(self.filepath)
-        self.report({'INFO'}, Tools.EXPORT_FINISHED)
+        Tools.local_update()
+        path, filename = os.path.split(self.filepath)
+        Tools.write_json_data(Tools.TEXTURES_KEYWORDS_DICT, filename, path)
+        self.report({'INFO'}, Tools.MESSAGES["Database_Export_Success"])
         return {"FINISHED"}
 
 
@@ -119,17 +111,17 @@ class DataImporter(bpy.types.Operator, ImportHelper):
     bl_idname = "pbr.import_data"
     bl_label = "Import User Masks"
 
-    # ExportHelper mixin class uses this
     filename_ext = ".json"
     filter_glob = bpy.props.StringProperty(default="*.json")
 
     def execute(self, context):
         if DataImporter.filename_ext in self.filepath:
-            Tools.read_textures(self.filepath)
-            self.report({'INFO'}, Tools.IMPORT_FINISHED)
+            path, filename = os.path.split(self.filepath)
+            Tools.TEXTURES_KEYWORDS_DICT = Tools.read_json_data(filename, path)
+            Tools.update_panel_masks()
+            self.report({'INFO'}, Tools.MESSAGES["Database_Import_Success"])
         else:
-            self.report({'ERROR'}, Tools.IMPORT_ERROR)
-
+            self.report({'ERROR'}, Tools.MESSAGES["Database_Import_Error"])
         return {"FINISHED"}
 
 
