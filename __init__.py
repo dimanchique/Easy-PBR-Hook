e2e579005ac742e40fa7c784ef5855bd71248443
dimanchique <ghettorunner@yandex.ru>
2022-02-13 10:59:06 +0300

    version updated

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import importlib
 bl_info = {
     "name": "Easy PBR Hook",
     "author": "Dmitry F.",
-    "version": (1, 6, 2),
+    "version": (1, 6, 5),
     "blender": (2, 80, 0),
     "location": "Properties > Material",
     "description": "Easy PBR Hook",
